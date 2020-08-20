1bf1cbf78654579ea5c80fb216d022b688a81657
Bruna Martini <abrunamartini@gmail.com>
2020-08-20 09:26:13 -0300

    work in progress

--- decision_tree.py
+++ decision_tree.py
@@ -2,51 +2,65 @@ import xml.etree.ElementTree as ET
 import os
 import numpy as np
 import csv
+import math
 
-# retorna o diretório de trabalho atual de um processo + a pasta onde está os dados dos elétros
-directory =  '/Users/brunamartini/code/power_signature' + '/ACS-F2'
+#  Ler arquivo CSV
+#  Calcular variáveis para arvore de decisão
 
-# gera os nomes dos arquivos em uma árvore de diretórios, percorrendo a árvore de cima para baixo ou de baixo para cima.
-list_dir = [x[0] for x in os.walk(directory)]
-list_dir = list_dir[1:]
-list_name = []
+def gain_of_information(variable, row_count, ent_p):
+  peso_1 = variable/row_count
+  peso_2 = (row_count-variable)/row_count
 
-# criar planilha e popular cabeçalho
-c = csv.writer(open("db_elet.csv", "w"))
-c.writerow(["Eletro", "phAngle_min", "phAngle_max", "Freq_min", "Freq_max", "ReactPower_min", "ReactPower_max", "Power_min", "Power_max", "Volts_min", "Volts_max", "Cur_min", "Cur_max"])
+  ent_f1 = -(math.log(1/variable))/math.log(variable)
+  ent_f2 = -(math.log(1/(row_count-variable)))/math.log((row_count-variable))
 
-for dirnames in list_dir:
-  list_filenames = [x[2] for x in os.walk(dirnames)][0]
-  for filenames in list_filenames:
-    # Se a extenção do aqruivo for .mat
-    if filenames.endswith(".mat"):
-      cnt = 0
-      list_mat_phAngle = []
-      list_mat_freq = []
-      list_mat_power = []
-      list_mat_reacPower = []
-      list_mat_rmsCur = []
-      list_mat_rmsVolt = []
-      for line in open(dirnames + '/' + filenames):
-        li=line.strip()
-        if not li.startswith("#"):
-          line_float = list(map(float, li.split(' ')))
-          cnt += 1
-          if cnt == 1:
-            list_mat_phAngle = line_float
-          elif cnt == 2:
-            list_mat_freq = line_float
-          elif cnt == 3:
-            list_mat_reacPower = line_float
-          elif cnt == 4:
-            list_mat_power = line_float
-          elif cnt == 5:
-            list_mat_rmsVolt = line_float
-          elif cnt == 6:
-            list_mat_rmsCur = line_float
-        if cnt != 6:
-          print(filenames);
+  gain = ent_p - (peso_1*ent_f1 + peso_2*ent_f2)
+  return gain
+class question:
+  def __init__(self, text, gain_of_information, position):
+    self.text = text
+    self.gain_of_information = gain_of_information
+    self.position = position
 
-      # salva valores em arquivo csv
-      c.writerow([filenames,np.min(list_mat_phAngle),np.max(list_mat_phAngle),np.min(list_mat_freq),np.max(list_mat_freq),np.min(list_mat_reacPower),np.max(list_mat_reacPower),np.min(list_mat_power),np.max(list_mat_power),np.min(list_mat_rmsVolt),np.max(list_mat_rmsVolt),np.min(list_mat_rmsCur),np.max(list_mat_rmsCur)])
+# calcula o número de linhas de um arquivo CSV (subtrai um para retirar o cabeçalho)
+row_count = sum(1 for line in open('db_elet.csv')) - 1
+# Cálcula entropia total da arvore
+ent_p = -(math.log(1/row_count))/math.log(row_count)
+
+frequencia = 0
+angulo = 0
+pot_r = 0
+pot = 0
+tensao = 0
+corrente = 0
+
+with open('db_elet.csv', 'r') as file:
+  reader = csv.reader(file)
+  for linha in reader:
+    # frequencia = 50
+    if float(linha[2]) == 50:
+      frequencia = frequencia + 1
+    # Angulo > 200
+    if float(linha[1]) > 200:
+      angulo = angulo + 1
+    # potencia reativa > 0
+    if float(linha[3]) > 0:
+      pot_r = pot_r + 1
+    # potencia > 50
+    if float(linha[4]) > 50:
+      pot = pot + 1
+    # tensão > 230
+    if float(linha[5]) > 230:
+      tensao = tensao + 1
+    # corrente > 0.1
+    if float(linha[6]) > 0.1:
+      corrente = corrente + 1
+
+# ganho de informação da frequencia
+frequencia = print(gain_of_information(frequencia, row_count, ent_p))
+angulo = print(gain_of_information(angulo, row_count, ent_p))
+pot_r = print(gain_of_information(pot_r, row_count, ent_p))
+tensao = print(gain_of_information(tensao, row_count, ent_p))
+corrente = print(gain_of_information(corrente, row_count, ent_p))
+
 
